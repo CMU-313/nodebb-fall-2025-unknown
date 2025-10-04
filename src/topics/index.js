17f600c4e67d513c42bf841c8c78a34333e7f95d
Alex Cheung <alexjakhangcheung@gmail.com>
2025-10-04 17:02:19 +0000

    feat: Edited getTopicsByTitleKeywords to include a fuzzy option

--- src/topics/index.js
+++ src/topics/index.js
@@ -330,8 +330,44 @@ Topics.search = async function (tid, term) {
 	return Array.isArray(result) ? result : result.ids;
 };
 
+// Created using Copilot
+function calculateEditDistance(a, b) {
+	if (a.length === 0) return b.length;
+	if (b.length === 0) return a.length;
+
+	const matrix = [];
+
+	for (let i = 0; i <= b.length; i++) {
+		matrix[i] = [i];
+	}
+
+	for (let j = 0; j <= a.length; j++) {
+		matrix[0][j] = j;
+	}
+
+	for (let i = 1; i <= b.length; i++) {
+		for (let j = 1; j <= a.length; j++) {
+			if (b.charAt(i - 1) === a.charAt(j - 1)) {
+				matrix[i][j] = matrix[i - 1][j - 1];
+			} else {
+				matrix[i][j] = Math.min(
+					matrix[i - 1][j - 1] + 1,
+					Math.min(
+						matrix[i][j - 1] + 1,
+						matrix[i - 1][j] + 1,
+					),
+				);
+			}
+		}
+	}
+
+	return matrix[b.length][a.length];
+}
+
+const EDIT_DISTANCE_PERCENT_ALLOWED = 0.2;
+
 // Created using Clause Sonnet 4
-Topics.getTopicsByTitleKeywords = async function (uid, keywords, start, stop) {
+Topics.getTopicsByTitleKeywords = async function (uid, keywords, start, stop, fuzzy = false) {
 	if (!keywords) {
 		throw new Error('[[error:invalid-keyword]]');
 	}
@@ -361,12 +397,22 @@ Topics.getTopicsByTitleKeywords = async function (uid, keywords, start, stop) {
 	
 	const keywordsLower = keywordArray.map(k => k.toLowerCase());
 	const matchedTids = [];
-	
+
 	for (const topic of topicsData) {
 		if (topic && topic.tid && topic.title) {
 			const title = topic.title.toLowerCase();
-			// Check if any of the keywords appears in the title
-			if (keywordsLower.some(keyword => title.includes(keyword))) {
+			const fuzzyMatchFunction = (keyword) => {
+				for (let i = 0; i <= title.length - keyword.length; i++) {
+					const substring = title.substring(i, i + keyword.length);
+					if (calculateEditDistance(substring, keyword) <= Math.floor(keyword.length * EDIT_DISTANCE_PERCENT_ALLOWED)) {
+						return true;
+					}
+				}
+				return false;
+			};
+			const matchFunction = keyword => title.includes(keyword);
+			if ((fuzzy && keywordsLower.some(fuzzyMatchFunction)) || 
+			(!fuzzy && keywordsLower.some(matchFunction))) {
 				matchedTids.push(topic.tid);
 			}
 		}
