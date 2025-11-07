b73c93eceea02158ea0f12b3cc2c57520d89ad26
kpark1208 <hyeonjap@andrew.cmu.edu>
2025-11-07 20:36:43 +0000

    fix : replaced replaced local host url to vm deployed IP

--- src/translate/index.js
+++ src/translate/index.js
@@ -7,7 +7,7 @@ const translatorApi = module.exports;
 translatorApi.translate = async function (postData) {
 //  Edit the translator URL below
 
-	const TRANSLATOR_API = 'http://172.17.0.3:5000';
+	const TRANSLATOR_API = 'http://128.2.220.229:5000';
 	try { 
 		const response = await fetch(TRANSLATOR_API + '/?content=' + encodeURIComponent(postData.content));
 		const data = await response.json();
