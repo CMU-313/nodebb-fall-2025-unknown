90f1797ab1e3227b6bc198c1b251424cbc2e33e2
Muskan Mankikar <muskanm@andrew.cmu.edu>
2025-11-07 02:51:43 +0000

    fix: fix indentation

--- src/translate/index.js
+++ src/translate/index.js
@@ -7,13 +7,13 @@ const translatorApi = module.exports;
 translatorApi.translate = async function (postData) {
 //  Edit the translator URL below
 
-  const TRANSLATOR_API = 'http://172.17.0.3:5000';
-  try { 
-    const response = await fetch(TRANSLATOR_API + '/?content=' + encodeURIComponent(postData.content));
-    const data = await response.json();
-    return [data.is_english, data.translated_content];
-  } catch(err) {
-    console.log('no need to translate');
-    return [true, postData.content];
-  }
+const TRANSLATOR_API = 'http://172.17.0.3:5000';
+    try { 
+        const response = await fetch(TRANSLATOR_API + '/?content=' + encodeURIComponent(postData.content));
+        const data = await response.json();
+        return [data.is_english, data.translated_content];
+    } catch(err) {
+        console.log('no need to translate');
+        return [true, postData.content];
+    }
 };
